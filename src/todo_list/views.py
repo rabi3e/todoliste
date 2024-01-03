a3f213a25c40e189bda626f09df3fb503fd68bf3
chatouirabie <chatouirabie@gmail.com>
2024-01-03 23:17:03 +0100

    get the list of items from data base

--- src/todo_list/views.py
+++ src/todo_list/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-
+from .models import List
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+
+    items = List.objects.all
+
+    return render(request, 'home.html', {'items': items})
 
 def about(request):
     return render(request, 'about.html', {})
