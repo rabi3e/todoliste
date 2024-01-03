cc76ffcc79dbedae5dd3ab012679849c6446eb02
chatouirabie <chatouirabie@gmail.com>
2024-01-03 22:50:15 +0100

    create list model

--- src/todo_list/models.py
+++ src/todo_list/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class List(models.Model):
+    item= models.CharField(max_length=200)
+    create_time = models.DateTimeField(auto_now=False, auto_now_add=True)
+    update = models.DateTimeField(auto_now=True, auto_now_add=False)
+    completed = models.BooleanField(default = False)
+
+    
+
+    class Meta:
+        verbose_name = "List"
+        verbose_name_plural = "Lists"
+
+    def __str__(self):
+        return self.item
