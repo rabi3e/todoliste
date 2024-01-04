b5e57b3f04b49eeb3fd5ad0d9597667f12353619
chatouirabie <chatouirabie@gmail.com>
2024-01-04 12:24:14 +0100

    add form.py + add data from the form

--- src/todo_list/views.py
+++ src/todo_list/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from .models import List
+from .forms import ListForm
+
 # Create your views here.
 def home(request):
 
-    items = List.objects.all
-
-    return render(request, 'home.html', {'items': items})
+    if request.method=="POST":
+        form = ListForm(request.POST)
+        if form.is_valid():
+            form.save()
+            items = List.objects.all
+            return render(request, 'home.html', {'items': items})
+    else :
+        items = List.objects.all
+        return render(request, 'home.html', {'items': items})
 
 def about(request):
     return render(request, 'about.html', {})
